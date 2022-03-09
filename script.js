20d1fa932c6759487ae586c39800ae2a78a87967
Matt92-dev <69991489+Matt92-dev@users.noreply.github.com>
2022-03-09 21:23:56 +0000

    Prices and discount change upon user input across all items

--- script.js
+++ script.js
@@ -1,26 +1,53 @@
-var currentTotal = 0;
-var itemAOfferCount = 0;
-var itemBOfferCount = 0;
-var globalItemCount = 0;
+//Item A Globals
 var itemA = 0.5;
+var itemAOfferCount = 0;
+var itemAOfferInt = 3;
+var itemAOfferAmount = .3;
+var noOfferA = true;
+
+//Item B Globals
 var itemB = 0.3;
+var itemBOfferCount = 0;
+var itemBOfferInt = 2;
+var itemBOfferAmount = .15;
+var noOfferB = true;
+
+//Item C Globals
 var itemC = 0.2;
-var itemD = 0.12;
+var itemCOfferCount = 0;
+var itemCOfferInt = 2;
+var itemCOfferAmount = .15;
+var noOfferC = true;
+
+//Item D Globals
+var itemD = 0.15;
+var itemDOfferCount = 0;
+var itemDOfferInt = 2;
+var itemDOfferAmount = .15;
+var noOfferD = true;
+
+
+var globalItemCount = 0;
+var currentTotal = 0;
 
 jQuery(document).ready(function () {
-    
 
     //Item A click function
     $('#item-a').click(function () {
-        itemAOfferCount += 1;
-        if (itemAOfferCount == 3) {
-            globalItemCount += 1;
-            itemAOfferCount = 0;
-            currentTotal += .3;
-            alert('3 for 2!');
-        } else {
+        if (noOfferA) {
             globalItemCount += 1;
             currentTotal += itemA;
+        } else {
+            itemAOfferCount += 1;
+            if (itemAOfferCount == itemAOfferInt) {
+                globalItemCount += 1;
+                itemAOfferCount = 0;
+                currentTotal += itemAOfferAmount;
+                alert('You have saved \u00A3' + ((itemA - itemAOfferAmount).toFixed(2)) + '!');
+            } else {
+                globalItemCount += 1;
+                currentTotal += itemA;
+            }
         }
         $('#item-count').text(globalItemCount);
         $('#total').text(currentTotal.toFixed(2));
@@ -28,15 +55,20 @@ jQuery(document).ready(function () {
 
     //Item B click function
     $('#item-b').click(function () {
-        itemBOfferCount += 1;
-        if (itemBOfferCount == 2) {
-            globalItemCount += 1;
-            itemBOfferCount = 0;
-            currentTotal += .15;
-            alert('2 for 1!');
-        } else {
+        if (noOfferB) {
             globalItemCount += 1;
             currentTotal += itemB;
+        } else {
+            itemBOfferCount += 1;
+            if (itemBOfferCount == itemBOfferInt) {
+                globalItemCount += 1;
+                itemBOfferCount = 0;
+                currentTotal += itemBOfferAmount;
+                alert('You have saved \u00A3' + ((itemB - itemBOfferAmount).toFixed(2)) + '!');
+            } else {
+                globalItemCount += 1;
+                currentTotal += itemB;
+            }
         }
         $('#item-count').text(globalItemCount);
         $('#total').text(currentTotal.toFixed(2));
@@ -44,17 +76,93 @@ jQuery(document).ready(function () {
 
     //Item C click function
     $('#item-c').click(function () {
-        currentTotal += itemC;
-        globalItemCount += 1;
+        if (noOfferC) {
+            globalItemCount += 1;
+            currentTotal += itemC;
+        } else {
+            itemCOfferCount += 1;
+            if (itemCOfferCount == itemCOfferInt) {
+                globalItemCount += 1;
+                itemCOfferCount = 0;
+                currentTotal += itemCOfferAmount;
+                alert('You have saved \u00A3' + ((itemC - itemCOfferAmount).toFixed(2)) + '!');
+            } else {
+                globalItemCount += 1;
+                currentTotal += itemC;
+            }
+        }
         $('#item-count').text(globalItemCount);
         $('#total').text(currentTotal.toFixed(2));
     });
 
     //Item D click function
     $('#item-d').click(function () {
-        currentTotal += itemD;
-        globalItemCount += 1;
+        if (noOfferD) {
+            globalItemCount += 1;
+            currentTotal += itemD;
+        } else {
+            itemDOfferCount += 1;
+            if (itemDOfferCount == itemDOfferInt) {
+                globalItemCount += 1;
+                itemDOfferCount = 0;
+                currentTotal += itemDOfferAmount;
+                alert('You have saved \u00A3' + ((itemD - itemDOfferAmount).toFixed(2)) + '!');
+            } else {
+                globalItemCount += 1;
+                currentTotal += itemD;
+            }
+        }
         $('#item-count').text(globalItemCount);
         $('#total').text(currentTotal.toFixed(2));
     });
+
+    //Item A price setup
+    $('#submit-item-info').click(function () {
+        getItemAInfo();
+        getItemBInfo();
+        getItemCInfo();
+        getItemDInfo();
+    })
+
 });
+
+function getItemAInfo() {
+        itemA = parseFloat($("#item-a-price").val());
+        itemAOfferInt = parseFloat($("#item-a-offer-amount").val());
+        itemAOfferAmount = parseFloat($("#item-a-offer-price").val());
+        if ($('#item-a-check').is(":checked")) {
+            noOfferA = true;
+        } else {
+            noOfferA = false;
+        }
+}
+function getItemBInfo() {
+        itemB = parseFloat($("#item-b-price").val());
+        itemBOfferInt = parseFloat($("#item-b-offer-amount").val());
+        itemBOfferAmount = parseFloat($("#item-b-offer-price").val());
+        if ($('#item-b-check').is(":checked")) {
+            noOfferB = true;
+        } else {
+            noOfferB = false;
+        }
+}
+function getItemCInfo() {
+        itemC = parseFloat($("#item-c-price").val());
+        itemCOfferInt = parseFloat($("#item-c-offer-amount").val());
+        itemCOfferAmount = parseFloat($("#item-c-offer-price").val());
+        if ($('#item-c-check').is(":checked")) {
+            noOfferC = true;
+        } else {
+            noOfferC = false;
+        }
+}
+function getItemDInfo() {
+        itemD = parseFloat($("#item-d-price").val());
+        itemDOfferInt = parseFloat($("#item-d-offer-amount").val());
+        itemDOfferAmount = parseFloat($("#item-d-offer-price").val());
+        if ($('#item-d-check').is(":checked")) {
+            noOfferD = true;
+        } else {
+            noOfferD = false;
+        }
+}
